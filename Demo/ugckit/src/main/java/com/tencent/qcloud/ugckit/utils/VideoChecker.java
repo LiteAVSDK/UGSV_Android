f6d1fc2a9880e7588b0c2a999e4b7761f186d4a8
aazgulhuang <aazgulhuang@tencent.com>
2025-03-26 10:39:24 +0800

    update to 12.4

--- Demo/ugckit/src/main/java/com/tencent/qcloud/ugckit/utils/VideoChecker.java
+++ Demo/ugckit/src/main/java/com/tencent/qcloud/ugckit/utils/VideoChecker.java
@@ -6,6 +6,7 @@ import android.media.MediaMetadataRetriever;
 import android.os.Build;
 import android.text.TextUtils;
 
+import com.tencent.liteav.base.util.LiteavLog;
 import com.tencent.qcloud.ugckit.R;
 import com.tencent.qcloud.ugckit.module.picker.data.TCVideoFileInfo;
 
@@ -13,25 +14,38 @@ import java.util.List;
 
 public class VideoChecker {
     public static boolean isVideoDamaged(Context context, TCVideoFileInfo info) {
-        if (info.getDuration() == 0) {
-            //数据库获取到的时间为0，使用Retriever再次确认是否损坏
-            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            try {
-                if (Build.VERSION.SDK_INT >= 29) {
-                    retriever.setDataSource(context, info.getFileUri());
-                } else {
-                    retriever.setDataSource(info.getFilePath());
-                }
-            } catch (Exception e) {
-                return true; //无法正常打开，也是错误
-            }
-            String duration =
-                    retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-            retriever.release();
-            if (TextUtils.isEmpty(duration)) return true;
-            return Integer.valueOf(duration) == 0;
+        if (info.getDuration() != 0) {
+            return false;
         }
-        return false;
+
+        //数据库获取到的时间为0，使用Retriever再次确认是否损坏
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            if (Build.VERSION.SDK_INT >= 29) {
+                retriever.setDataSource(context, info.getFileUri());
+            } else {
+                retriever.setDataSource(info.getFilePath());
+            }
+        } catch (Exception e) {
+            return true; //无法正常打开，也是错误
+        }
+        String duration =
+                retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        try {
+            retriever.release();
+        } catch (Throwable throwable) {
+            LiteavLog.i("VideoChecker",throwable.toString());
+        }
+
+        try {
+            if (!TextUtils.isEmpty(duration)) {
+                return Integer.parseInt(duration) == 0;
+            }
+        } catch (Throwable throwable) {
+            LiteavLog.i("VideoChecker",throwable.toString());
+        }
+
+        return true;
     }
 
     public static boolean isVideoDamaged(Context context, List<TCVideoFileInfo> list) {
