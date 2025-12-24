10fee28cd6d1acc0e66296d056798a8ef1b97092
aazgulhuang <aazgulhuang@tencent.com>
2025-12-24 14:30:28 +0800

    update to 13.0

--- Demo/ugckit/src/main/java/com/tencent/qcloud/ugckit/module/upload/impl/TVCConstants.java
+++ Demo/ugckit/src/main/java/com/tencent/qcloud/ugckit/module/upload/impl/TVCConstants.java
@@ -5,7 +5,7 @@ package com.tencent.qcloud.ugckit.module.upload.impl;
  * 视频上传常量定义
  */
 public class TVCConstants {
-    public static final String TVCVERSION = "12.9.0.19468";
+    public static final String TVCVERSION = "13.0.0.19660";
 
     /**
      * evn for DOMESTIC
